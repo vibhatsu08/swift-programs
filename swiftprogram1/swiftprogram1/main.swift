852267e7b0a3232c07d06c20853eac1b7cade8c5
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-06 19:52:04 +0530

    swift program 1
    Write a Swift program to compute the sum of the two integers. If the values are equal return the triple their sum

---
+++ swiftprogram1/swiftprogram1/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  swiftprogram1
+//
+//  Created by Vedant Mistry on 06/09/21.
+//
+
+import Foundation
+
+//Write a Swift program to compute the sum of the two integers. If the values are equal return the triple their sum
+var num1: Int = 12
+var num2: Int = 13
+var result: Int = 0
+if(num1 == num2){
+    result = num1 + num2 + num2
+    print(result)
+}
+else{
+    result = num1 + num2
+    print(result)
+}
+
+
+
