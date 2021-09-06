8b6a8a6e0de1deae21d9b62a201102dbe385889b
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-06 20:00:21 +0530

    swift program 3
    Write a Swift program that accept two integer values and return true if one of them is 20 or if their sum is 20

---
+++ swiftprogram3/swiftprogram3/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  swiftprogram3
+//
+//  Created by Vedant Mistry on 06/09/21.
+//
+
+import Foundation
+//Write a Swift program that accept two integer values and return true if one of them is 20 or if their sum is 20
+
+var num1: Int = 10
+var num2: Int = 10
+var sum: Int = num1 + num2
+if(num1 == 20 || sum == 20){
+    print("true")
+}
+else{
+    print("false")
+}
