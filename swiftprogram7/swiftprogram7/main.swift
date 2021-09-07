81064ee91e232b3ec873c14f52f11f6d661ef53e
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-07 11:29:28 +0530

    swift program 7

---
+++ swiftprogram7/swiftprogram7/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  swiftprogram7
+//
+//  Created by Vedant Mistry on 07/09/21.
+//
+
+import Foundation
+//swift program to change the first and the last character of a given string
+func changeCharacters(str: String){
+    var countStr = str.count
+    var newWord = str
+    
+    if countStr > 0{
+        newWord = str.startIndex = str.endIndex
+    }
+}
+
